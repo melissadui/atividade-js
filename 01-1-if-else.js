4f45f61c6b2a95a83806ab4e11f3fcd775e23d29
unknown <melissaduiliosilva@gmail.com>
2025-09-28 20:24:52 -0300

    verificar número positivo ou negativo com if else

---
+++ 01-1-if-else.js
@@ -0,0 +1,12 @@
+// definir variável com um número, verificar se é positivo, negativo ou igual a zero. Mostrar no console com resultado.
+
+// if(condicao) {};
+
+let numero = 5;
+if (numero > 0) {
+    console.log(`O número é positivo.`)
+} else if (numero < 0) {
+    console.log(`O número é negativo.`)
+} else {
+    console.log(`O número é igual a zero.`)
+}
