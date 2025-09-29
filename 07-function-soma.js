8894408d3791c0493155f979d6abf10e4a92b070
unknown <melissaduiliosilva@gmail.com>
2025-09-28 21:26:17 -0300

    soma de números com function

---
+++ 07-function-soma.js
@@ -0,0 +1,8 @@
+// função chamada somar que receba dois números como parâmetros e retorne a soma entre eles.
+
+function somar(a, b) {
+    let soma = a + b;
+    console.log(`A soma de ${a} e ${b} é ${soma}`);
+}
+
+somar(20, 5);
