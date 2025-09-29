f04bb99510acaca5181d309b2fd91937e501ae8a
unknown <melissaduiliosilva@gmail.com>
2025-09-28 21:00:38 -0300

    laco for tabuada de 5

---
+++ 04-laco-for-tabuada.js
@@ -0,0 +1,11 @@
+// exiba a tabuada de multiplicação de um número definido em uma variável, usando for para mostrar os resultados de 1 a 10.
+
+const numero = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(numero * i);
+// }
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} x ${i} é ${numero * i}`);
+};
